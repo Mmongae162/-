080cc7f440eb002ea37bb9d2698436dd92a5f8c0
Mmongae162 <fire49096784@gmail.com>
2026-04-04 23:59:45 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
+const screenIntro = document.getElementById("screen-intro");
+const screenStart = document.getElementById("screen-start");
 
+const toStartScreenButton = document.getElementById("to-start-screen");
+const backToIntroButton = document.getElementById("back-to-intro");
+const startQuizButton = document.getElementById("start-quiz-button");
+
+function showScreen(screenId) {
+  const allScreens = document.querySelectorAll(".screen");
+  allScreens.forEach((screen) => {
+    screen.classList.remove("active");
+  });
+
+  document.getElementById(screenId).classList.add("active");
+}
+
+toStartScreenButton.addEventListener("click", () => {
+  showScreen("screen-start");
+});
+
+backToIntroButton.addEventListener("click", () => {
+  showScreen("screen-intro");
+});
+
+startQuizButton.addEventListener("click", () => {
+  alert("다음 단계에서 학년 선택 화면으로 연결할 예정입니다.");
+});
